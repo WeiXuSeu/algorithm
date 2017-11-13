59d8fef66963ba48d9de0ecad33958e285ff3c38
Harvy Xu <xuweiroot@163.com>
2017-11-13 23:27:19 +0800

    add 95_ValidateBinarySearchTree.cpp

---
+++ binaryTree/95_ValidateBinarySearchTree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+
+class Solution {
+public:
+    /*
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    bool isValidBST(TreeNode * root) {
+        // write your code here
+        vector<int> vec;
+        if(root == NULL)
+            return true;
+        inorderTraversal(root, vec);
+        bool result = true;
+        for(int i = 0; i < vec.size() - 1; i++) {
+            if(vec[i] >= vec[i+1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+    
+    void inorderTraversal(TreeNode * root, vector<int> &vec) {
+        if(root == NULL) {
+            return;
+        }
+        inorderTraversal(root->left, vec);
+        vec.push_back(root->val);
+        inorderTraversal(root->right, vec);
+    } 
+};
